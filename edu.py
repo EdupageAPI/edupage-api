0aad57c6d10af7b9f604a3127f570f3940ab0a18
Ivan Hrabcak <ivan.hrabcak@gmail.com>
2020-10-20 21:09:23 +0200

    add Edupage.get_students

--- edu.py
+++ edu.py
@@ -1,4 +1,4 @@
-import requests, json, datetime
+import requests, json, datetime, pprint
 
 class EduDate:
 	def __init__(self, year, day, month):
@@ -6,11 +6,10 @@ class EduDate:
 		self.day = day
 		self.month = month
 
-	def __init__(self, formatted_date):
+	@staticmethod
+	def from_formatted_date(formatted_date):
 		s = formatted_date.split("-")
-		self.year = s[0]
-		self.month = s[1]
-		self.day = s[2]
+		return EduDate(s[0], s[1], s[2])
 
 	def __str__(self):
 		return "%s-%s-%s" % (self.year, self.month, self.day)
@@ -30,6 +29,19 @@ class Ids:
 
 	def IdToSubject(self, id):
 		return self.dbi.get("subjects").get(id).get("short")
+
+class EduStudent:
+	def __init__(self, gender, firstname, lastname, student_id, number, is_out):
+		self.gender = gender
+		self.firstname = firstname
+		self.lastname = lastname
+		self.fullname = firstname + " " + lastname
+		self.id = int(student_id)
+		self.number_in_class = int(number) 
+		self.is_out = is_out
+	
+	def __str__(self):
+		return "{gender: %s, name: %s, id: %d, number: %s, is_out: %s" % (self.gender, self.fullname, self.id, self.number_in_class, self.is_out)
 
 class EduLength:
 	def __init__(self, start, end):
@@ -57,8 +69,13 @@ class Edupage:
 		response = requests.post("https://portal.edupage.org/index.php?jwid=jw2&module=Login", parameters)
 		if "wrongPassword" in response.url:
 			return False
-		js_json = response.content.decode().split("$j(document).ready(function() {")[1].split(");")[0].replace("\t", "").split("userhome(")[1].replace("\n", "").replace("\r", "")
-		self.data = json.loads(js_json)   
+		try:
+			js_json = response.content.decode().split("$j(document).ready(function() {")[1].split(");")[0].replace("\t", "").split("userhome(")[1].replace("\n", "").replace("\r", "")
+		except TypeError:
+			return False
+		except IndexError:
+			return False
+		self.data = json.loads(js_json)
 		self.is_logged_in = True
 		self.ids = Ids(self.data)
 		return True
@@ -101,13 +118,35 @@ class Edupage:
 
 			
 		return subjects
+	
+	def get_students(self):
+		try:
+			students = self.data.get("dbi").get("students")
+		except Exception:
+			return None
+		if students == None:
+			return []
+		
+		result = []
+		for student_id in students:
+			student_data = students.get(student_id)
+			gender = student_data.get("gender")
+			firstname = student_data.get("firstname")
+			lastname = student_data.get("lastname")
+			is_out = student_data.get("isOut")
+			number_in_class = student_data.get("numberinclass")
+
+
+			student = EduStudent(gender, firstname, lastname, student_id, number_in_class, is_out)
+			result.append(student)
+		return result
 
 
 def main():
 	datet = datetime.datetime.now()
-	date = EduDate(str(datet).split(" ")[0])
+	date = EduDate.from_formatted_date(str(datet).split(" ")[0])
 
-	edu = Edupage(input("Username? "), input("Password? "))
+	edu = Edupage(input("Username? "),  input("Password? "))
 	was_successfull = edu.login()
 	if was_successfull:
 		print("Login successfull!")
@@ -117,13 +156,17 @@ def main():
 	timetable = edu.get_timetable(date)
 	if timetable == None:
 		print("No timetable for this date.")
-		return
-	for lesson in timetable:
-		print(lesson.name)
-		print(lesson.teacher)
-		print(lesson.classroom)
-		print(str(lesson.length))
-		print("\n")
+	else:
+		for lesson in timetable:
+			print(lesson.name)
+			print(lesson.teacher)
+			print(lesson.classroom)
+			print(str(lesson.length))
+			print("\n")
+	students = edu.get_students()
+	print(students)
+	for student in students:
+		print(student)
 
 if __name__ == "__main__":
 	main()

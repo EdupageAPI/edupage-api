d865a5eaed8dff39cca8f15805ba0c68c1f30e0b
Ivan Hrabcak <ivan@hrabcak.eu>
2021-02-12 20:59:50 +0100

    Migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = "edupage_api",
-    packages = ["edupage_api"],
-    version = "0.3",
-    license = "GNU GPLv3",
-    description = "A python library for accessing your Edupage account",
-    author = "ivanhrabcak",
-    author_email = "ivan@hrabcak.eu",
-    url = "https://github.com/ivanhrabcak/edupage-api",
-    download_url = "https://github.com/ivanhrabcak/edupage-api/archive/0.3.tar.gz",
-    keywords = ["edupage", "edupage api", "library"],
-    install_requires = ["requests"],
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
-    ]
-)
+setuptools.setup()

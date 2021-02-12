5082dad7fa944780a8b416acf7e3b916f2380b0b
Ivan Hrabcak <ivan@hrabcak.eu>
2021-02-12 20:09:57 +0100

    Correct dependencies

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 setup(
     name = "edupage_api",
     packages = ["edupage_api"],
-    version = "0.1",
+    version = "0.2",
     license = "GNU GPLv3",
     description = "A python library for accessing your Edupage account",
     author = "ivanhrabcak",
     author_email = "ivan@hrabcak.eu",
     url = "https://github.com/ivanhrabcak/edupage-api",
-    download_url = "https://github.com/ivanhrabcak/edupage-api/archive/0.1.tar.gz",
+    download_url = "https://github.com/ivanhrabcak/edupage-api/archive/0.2.tar.gz",
     keywords = ["edupage", "edupage api", "library"],
-    install_requires = ["requests", "datetime", "json", "pprint"],
+    install_requires = ["requests"],
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",

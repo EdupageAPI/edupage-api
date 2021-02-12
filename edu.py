101208b0d846dc7b7cf023698f8b73d7bc60d8d5
Ivan Hrabcak <ivan@hrabcak.eu>
2021-02-12 18:58:21 +0100

    Added a way of getting grade events

--- edu.py
+++ edu.py
@@ -36,23 +36,40 @@ class EduDate:
 	def __str__(self):
 		return "%s-%s-%s" % (self.year, self.month, self.day)
 
-class Ids:
+class GradeUtil:
+	def __init__(self, grade_data):
+		self.data = grade_data
+	
+	def id_to_teacher(self, teacher_id):
+		teachers = self.data.get("ucitelia")
+
+		# this dict contains data about the 
+		# date when this teacher was employed
+		# or when will he/she retire / is planned quit this job
+		# (datefrom, dateto) 
+		teacher = teachers.get(teacher_id) 
+
+		teacher_name = teacher.get("firstname") + " " + teacher.get("lastname")
+		
+		return teacher_name 
+
+class IdUtil:
 	def __init__(self, data):
 		self.data = data
 		self.dbi = data.get("dbi")
 	
-	def IdToClass(self, c_id):
+	def id_to_class(self, c_id):
 		return self.dbi.get("classes").get(c_id).get("name")
 
-	def IdToTeacher(self, t_id):
+	def id_to_teacher(self, t_id):
 		teacher_data = self.dbi.get("teachers").get(t_id)
 		teacher_full_name = teacher_data.get("firstname") + " " + teacher_data.get("lastname")
 		return teacher_full_name
 
-	def IdToClassroom(self, c_id):
+	def id_to_classroom(self, c_id):
 		return self.dbi.get("classrooms").get(c_id).get("short")
 
-	def IdToSubject(self, s_id):
+	def id_to_subject(self, s_id):
 		return self.dbi.get("subjects").get(s_id).get("short")
 
 class EduHomework:
@@ -97,18 +114,18 @@ class EduNews:
 	def __str__(self):
 		return f'{self.text}'
 
-class EduGrade:
-	def __init__(self, teacher, text, subject, grade, action, grade_id, datetime_added):
+# This is just a message that a grade has been received
+# and it contains no information about what the grade is.
+class EduGradeEvent:
+	def __init__(self, teacher, title, subject, average, weight, datetime_added):
 		self.teacher = teacher
-		self.text = text
+		self.title = title
 		self.subject = subject
-		self.grade = grade
-		self.action = action
-		self.id = grade_id
+		self.average = average
 		self.datetime_added = EduDateTime.from_formatted_datetime(datetime_added)
 	
 	def __str__(self):
-		return f'{self.text}'
+		return f'{self.title}'
 
 class EduStudent:
 	def __init__(self, gender, firstname, lastname, student_id, number, is_out):
@@ -173,7 +190,7 @@ class Edupage:
 		self.headers = response.headers
 		self.data = json.loads(js_json)
 		self.is_logged_in = True
-		self.ids = Ids(self.data)
+		self.ids = IdUtil(self.data)
 		return True
 	
 	def get_available_timetable_dates(self):
@@ -207,13 +224,13 @@ class Edupage:
 				return subjects
 			
 			subject_id = subj.get("subjectid")
-			subject_name = self.ids.IdToSubject(subject_id)
+			subject_name = self.ids.id_to_subject(subject_id)
 			
 			teacher_id = subj.get("teacherids")[0]
-			teacher_full_name = self.ids.IdToTeacher(teacher_id)
+			teacher_full_name = self.ids.id_to_teacher(teacher_id)
 
 			classroom_id = subj.get("classroomids")[0]
-			classroom_number = self.ids.IdToClassroom(classroom_id)
+			classroom_number = self.ids.id_to_classroom(classroom_id)
 
 			start = subj.get("starttime")
 			end = subj.get("endtime")
@@ -235,7 +252,7 @@ class Edupage:
 		if items == None:
 			return None
 		
-		ids = Ids(self.data)
+		ids = IdUtil(self.data)
 
 		homework = []
 		for item in items:
@@ -256,9 +273,9 @@ class Edupage:
 
 			event_id = data.get("superid")
 
-			class_name = ids.IdToClass(data.get("triedaid"))
+			class_name = ids.id_to_class(data.get("triedaid"))
 
-			subject = ids.IdToSubject(data.get("predmetid"))
+			subject = ids.id_to_subject(data.get("predmetid"))
 
 			timestamp = item.get("timestamp")
 
@@ -289,7 +306,11 @@ class Edupage:
 			news.append(news_message)
 		
 		return news
-	
+
+	# this method will soon be removed
+	# because all messages will be
+	# handled in some other way
+	"""
 	def get_grade_messages(self):
 		if not self.is_logged_in:
 			return None
@@ -298,7 +319,7 @@ class Edupage:
 		if items == None:
 			return None
 
-		ids = Ids(self.data)
+		ids = IdUtil(self.data)
 
 		messages = []
 		for item in items:
@@ -312,7 +333,7 @@ class Edupage:
 			data = json.loads(item.get("data"))
 			subject_id = list(data.keys())[0]
 
-			subject = ids.IdToSubject(subject_id)
+			subject = ids.id_to_subject(subject_id)
 
 			grade_data = data.get(subject_id)[0]
 			
@@ -320,10 +341,11 @@ class Edupage:
 			grade = grade_data.get("data")
 			action = grade_data.get("akcia")
 
-			edugrade = EduGrade(teacher, text, subject, grade, action, grade_id, timestamp)
+			edugrade = EduGradeMessage(teacher, text, subject, grade, action, grade_id, timestamp)
 			messages.append(edugrade)
 		
 		return messages
+	"""
 	
 	def get_grade_data(self):
 		response = self.session.get(f"https://{self.school}.edupage.org/znamky")
@@ -332,25 +354,38 @@ class Edupage:
 									.split(".znamkyStudentViewer(")[1] \
 									.split(");\r\n\t\t});\r\n\t\t</script>")[0])
 
-	# def get_grades(self):
-	# 	grade_data = self.get_grade_data()
+	def get_received_grade_events(self):
+		grade_data = self.get_grade_data()
 
-	# 	providers = grade_data.get("vsetkyUdalosti")
-	# 	events = providers.get("edupage")
-	# 	for event_id in events:
-	# 		event = events.get(event_id)
+		util = GradeUtil(grade_data)
+		id_util = IdUtil(self.data)
 
-	# 		if event.get("stav") == None: # wtf fake grades?
-	# 			continue
-	# 		elif event.get("priemer") == None: # ??? is this correct?
-	# 			continue
+		received_grade_events = []
 
-	# 		name = event.get("p_meno")
-	# 		average = event.get("priemer")
-	# 		timestamp = event.get("timestamp")
+		providers = grade_data.get("vsetkyUdalosti")
+		events = providers.get("edupage")
+		for event_id in events:
+			event = events.get(event_id)
 
+			if event.get("stav") == None:
+				continue
 
+			name = event.get("p_meno")
+			average = event.get("priemer")
+			timestamp = event.get("timestamp")
 
+			weight = event.get("p_vaha")
+
+			teacher_id = event.get("UcitelID")
+			teacher = util.id_to_teacher(teacher_id)
+
+			subject_id = event.get()
+			subject = id_util.id_to_subject(subject_id)
+
+			event = EduGradeEvent(teacher, name, subject, average, weight, timestamp)
+			received_grade_events.append(event)
+		
+		return received_grade_events
 	
 	def get_students(self):
 		try:
@@ -391,7 +426,6 @@ if __name__ == "__main__":
 
 """
 TODO:
-	- Grade averages -> where are they even stored? ---> almost done
 	- All message types
 	- a way to wait for new messages/news/grades... listeners?
 """

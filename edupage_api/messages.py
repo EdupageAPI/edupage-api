7f135ff74c6b391c3c18e2ce79614a50048b48b4
freegamerskids <60486718+freegamerskids@users.noreply.github.com>
2021-04-27 16:22:55 +0200

    Update messages.py

--- edupage_api/messages.py
+++ edupage_api/messages.py
@@ -4,14 +4,14 @@ from edupage_api.date import EduDate, EduDateTime
 class EduHomework:
     def __init__(self, due_date, subject, groups, title, description, event_id,
                  class_name, datetime_added):
-        self.due_date = EduDate.from_formatted_date(due_date)
+        self.due_date = EduDate.fromFormattedDate(due_date)
         self.subject = subject
         self.groups = groups
         self.title = title
         self.description = description
         self.event_id = event_id
         self.class_name = class_name
-        self.datetime_added = EduDateTime.from_formatted_datetime(
+        self.datetime_added = EduDateTime.fromFormattedDatetime(
             datetime_added)
 
     def __str__(self):
@@ -21,7 +21,7 @@ class EduHomework:
 class EduNews:
     def __init__(self, text, date_added, author, recipient):
         self.text = text
-        self.date_added = EduDateTime.from_formatted_datetime(date_added)
+        self.date_added = EduDateTime.fromFormattedDatetime(date_added)
         self.author = author
         self.recipient = recipient
 

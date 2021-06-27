df184e2f88f87a0a8a4f6f5fa0f017f5b05654e6
Ivan Hrabcak <ivan@hrabcak.eu>
2021-06-27 12:22:33 +0200

    Fix `EduNotification.id`, add reactions support

--- edupage_api/messages.py
+++ edupage_api/messages.py
@@ -1,5 +1,6 @@
 from edupage_api.date import EduDate, EduExactDateTime
 from enum import Enum
+import json
 
 
 class EduHomework:
@@ -64,24 +65,51 @@ class NotificationType(Enum):
 
 
 class EduNotification:
-	def __init__(self, notification_id, event_type, author, recipient, text, date_added, attachments, subject, name,
-                 due_date, grade, start, end, duration, event_type_name):
-		self.id = id
-		self.event_type = event_type
-		self.author = author
-		self.recipient = recipient
-		self.text = text
-		self.date_added = EduExactDateTime.from_formatted_string(date_added)
-		self.attachments = attachments
-		self.subject = subject
-		self.name = name
-		self.due_date = EduDate.from_formatted_string(due_date)
-		self.grade = grade
-		self.start = EduDate.from_formatted_string(start)
-		self.end = EduDate.from_formatted_string(end)
-		self.duration = duration
-		self.event_type_name = event_type_name
-		
-		def __str__(self):
-			return f'{self.event_type}, {self.text}'
+    def __init__(self, notification_id, event_type, author, recipient, text, date_added, attachments, subject, name,
+                    due_date, grade, start, end, duration, event_type_name):
+        self.id = notification_id
+        self.event_type = event_type
+        self.author = author
+        self.recipient = recipient
+        self.text = text
+        self.date_added = EduExactDateTime.from_formatted_string(date_added)
+        self.attachments = attachments
+        self.subject = subject
+        self.name = name
+        self.due_date = EduDate.from_formatted_string(due_date)
+        self.grade = grade
+        self.start = EduDate.from_formatted_string(start)
+        self.end = EduDate.from_formatted_string(end)
+        self.duration = duration
+        self.event_type_name = event_type_name
+
+    def __str__(self):
+        return f'{self.event_type}, {self.text}'
+    
+    def __add_reaction(self, edupage, state):
+        request_url = f"https://{edupage.school}.edupage.org/timeline/?akcia=createConfirmation"
+
+        data = {
+            "groupid": self.id,
+            "confirmType": "like",
+            "val": state
+        }
+
+        response = edupage.session.post(request_url, data = data)
+
+        try:
+            response_data = json.loads(response.content.decode())
+            if response_data.get("status") == "ok":
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def like(self, edupage):
+        return self.__add_reaction(edupage, "1")
+    
+    def remove_like(self, edupage):
+        return self.__add_reaction(edupage, "0")
+
   

27e16a9f4591b824388e707b264e79f6aa2404dd
BelKed <66956532+BelKed@users.noreply.github.com>
2022-02-14 20:45:55 +0100

    Simplify lunch example

--- examples/lunch.py
+++ examples/lunch.py
@@ -1,4 +1,3 @@
-import sys
 from datetime import datetime
 
 from edupage_api import Edupage
@@ -10,16 +9,15 @@ today = datetime.now()
 lunch = edupage.get_lunches(today)
 
 if lunch is None:
-    print("No lunch choices for today yet!")
-    sys.exit(0)
+    print(f"No lunch choices for today ({today.date()}) yet!")
+else:
+    for menu_index_str in lunch.chooseable_menus:
+        menu_index = int(menu_index_str)
+        print(f"[{menu_index_str}] {lunch.menus[menu_index].name}")
 
-for menu_index_str in lunch.chooseable_menus:
-    menu_index = int(menu_index_str)
-    print(f"[{menu_index_str}] {lunch.menus[menu_index].name}")
-
-lunch_n = input("Which lunch do you want? ")
-while lunch_n not in lunch.chooseable_menus:
     lunch_n = input("Which lunch do you want? ")
+    while lunch_n not in lunch.chooseable_menus:
+        lunch_n = input("Which lunch do you want? ")
 
-lunch.choose(edupage, int(lunch_n))
-print("Done!")
+    lunch.choose(edupage, int(lunch_n))
+    print("Done!")

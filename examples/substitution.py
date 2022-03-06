ca0b3fa5058a7d0fb826e3306fda4f1e8aeb128c
Ivan Hrabcak <ivan@hrabcak.eu>
2022-03-06 22:12:48 +0100

    Add substitution example

---
+++ examples/substitution.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+
+from edupage_api import Edupage
+
+edupage = Edupage()
+edupage.login_auto("Username (or e-mail)", "Password")
+
+tomorrow = datetime.now()
+missing_teachers = edupage.get_missing_teachers(tomorrow)
+
+print(f"There are {len(missing_teachers)} missing tomorrow!")
+
+print()
+
+print("Teachers missing:", end=" ")
+for i, teacher in enumerate(missing_teachers):
+    if i != 0:
+        print(", ", end="")
+    
+    print(f"{teacher.name}", end="")
